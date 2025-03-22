eacb51e6ffd0f06784f67ec47e3c6d0f975814b2
Yasaman Parhizkar <yasaman.parhizkar@gmail.com>
2025-03-22 14:47:41 -0400

    specific profile

--- post_on_linkedin.py
+++ post_on_linkedin.py
@@ -1,25 +1,52 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from browser_use import Agent
+from browser_use import Agent, Controller
+from browser_use.browser.browser import Browser, BrowserConfig
 from pydantic import SecretStr
 import os
 from dotenv import load_dotenv
 import asyncio
+from dataclasses import dataclass
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
+@dataclass
+class Config:
+    gemini_api_key: str
+    base_url: str
+    chrome_path: str = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    headless: bool = False
+    model: str = "gemini-2.0-flash-exp"
+    
 
-# Initialize the model
-llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(os.getenv('GEMINI_API_KEY')))
+# setup configs
+config = Config(
+        gemini_api_key=os.getenv("GEMINI_API_KEY"),
+        base_url="https://www.linkedin.com/in/yasamanparhizkar/",
+    )
+
+
+# initialize the model
+llm = ChatGoogleGenerativeAI(model=config.model, api_key=SecretStr(config.gemini_api_key))
 print(llm)
 
+browser = Browser(
+        config=BrowserConfig(
+            headless=config.headless,
+            chrome_instance_path=config.chrome_path
+        )
+    )
+
+controller = Controller()
+
 async def main():
-    # Create agent with the model
+    # create agent with the model
     agent = Agent(
         task="Compare the price of gpt-4o and DeepSeek-V3",
-        llm=llm
+        llm=llm,
+        browser=browser,
+        controller=controller,
     )
     print(agent)
-    await agent.run()
+    history = await agent.run()
+    print(f"RESULT: {history}")
 
 asyncio.run(main())
