9cbabacce2b4cc5bf38de2de3777fb344f791274
Yasaman Parhizkar <yasaman.parhizkar@gmail.com>
2025-03-22 15:18:17 -0400

    moved everything to backend

--- backend/post_on_linkedin.py
+++ backend/post_on_linkedin.py
@@ -12,6 +12,7 @@ load_dotenv()
 class Config:
     gemini_api_key: str
     base_url: str
+    post: str
     chrome_path: str = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
     headless: bool = False
     model: str = "gemini-2.0-flash-exp"
@@ -20,13 +21,13 @@ class Config:
 # setup configs
 config = Config(
         gemini_api_key=os.getenv("GEMINI_API_KEY"),
-        base_url="https://www.linkedin.com/in/yasamanparhizkar/",
+        base_url="https://www.linkedin.com/feed/",
+        post="Hello World!"
     )
 
 
 # initialize the model
 llm = ChatGoogleGenerativeAI(model=config.model, api_key=SecretStr(config.gemini_api_key))
-print(llm)
 
 browser = Browser(
         config=BrowserConfig(
@@ -40,12 +41,22 @@ controller = Controller()
 async def main():
     # create agent with the model
     agent = Agent(
-        task="Compare the price of gpt-4o and DeepSeek-V3",
+        task=f"""Navigate to LinkedIn and post a message with attached images.
+
+        Here are the steps you need to follow:
+        1. Navigate to {config.base_url}
+        2. Click on the text bar saying "Share a post".
+        3. In the pop up window, type the following message: "{config.post}"
+        4. Click on the photo icon at the bottom of the pop up window; this icon says "Add a photo" when you hover above it.
+        5. In the new window, navigate to the following path: Files/Career/GenAIGenesis_2025/pocketpr/images
+        6. In the current directory, select all image files and hit "open" at the bottom of the window.
+        7. Click "Next" in Editor window.
+        8. Stop here; Do NOT post the message.
+        """,
         llm=llm,
         browser=browser,
         controller=controller,
     )
-    print(agent)
     history = await agent.run()
     print(f"RESULT: {history}")
 
