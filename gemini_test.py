63aa289304d6388f1489ed441f5be59d0771117c
DrabbleCoding <50883868+DrabbleCoding@users.noreply.github.com>
2025-03-22 21:19:19 -0400

    Update gemini_test.py
    updated the prompt: sample hardcoded answers to questions

--- gemini_test.py
+++ gemini_test.py
@@ -54,9 +54,24 @@ for q in questions:
 
 # print(response.text)
 
+"""
+full prompt used for Gemini:
+
+
+I would like you to create a LinkedIn post that has a friendly, exciting and fresh tone with the use of emojis when necessary. I also would like the text to be in paragraph form, with space in between the hashtags and the main text. I'd also like some space between the title, date, and the main paragraph. Title and date should be on one line at the top. Here is the information you need to know:
+
+    Q: What is the name of the event? A: GenAI Genesis
+    Q: What was the time and place of the event? A: UofT, March 21-23
+    Q: What was the purpose of your attendance and your role in the event? A: Attended as a hacker to network, to challenge myself, and to learn lots from peers.
+    Q: What were your key takeaways or insights you gained? A: learned about lots of new tools, networked with professionals in the AI and software engineering fields, and produced 
+    Q: What was the main theme or purpose of the event? Main theme was AI for Human Empowerment
+    "How would you like to frame your call to action?", Ask the readers for their thoughts, or if anyone else attended the event
+    "Would you like hashtags at the end of your post for greater reach?" yes
+
+"""
 response = client.models.generate_content( #change to generate_content_stream later
     model="gemini-2.0-flash",
-    contents="I want you to write me a linkedin post about an event I went to. here are the information that you need to know about the event." + str(responses_list)
+    contents="I would like you to create a LinkedIn post that has a friendly, exciting and fresh tone with the use of emojis when necessary. I also would like the text to be in paragraph form, with space in between the hashtags and the main text. I'd also like some space between the title, date, and the main paragraph. Title and date should be on one line at the top. Here is the information you need to know:" + str(responses_list)
 
 )
 print(response)
